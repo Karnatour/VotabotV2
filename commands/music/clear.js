be865db06e5293310baf5be561eb6baa7fb5cc51
Vojtěch Šokala <vojtasokala@gmail.com>
2025-06-06 19:09:37 +0200

    Clear command improvements

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -9,10 +9,13 @@ module.exports = {
         try {
             let queue
             queue = queueMap.get(interaction.guildId.toString());
+            if (!queue || queue.size() <= 1) {
+                return await interaction.reply("Queue obsahuje jen jednu skladbu nebo je prázdná.");
+            }
             queue.clearQueue();
-            await interaction.reply("Queue cleared");
+            await interaction.reply("Queue vymazána");
         } catch (e) {
-            await interaction.reply("Fronta je prazdna");
+            await interaction.reply("Error při mazání queue");
         }
     },
 };
